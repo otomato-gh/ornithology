a5e981de46d915e874a27328e8dad604b93c2227
Anton Weiss <ant.weiss@gmail.com>
2018-01-23 15:43:07 +0200

    more info in version verifier

--- tests/it/version_verify.sh
+++ tests/it/version_verify.sh
@@ -3,8 +3,11 @@ canary_version=$(grep \'version\': /tests/api.py | cut -d \' -f 4)
 version=test
 count=100
 while [ $canary_version != $version ];
- do version=$(curl http://35.224.1.71:5000/version)
+ do version=$(curl http://ornithology:5000/version)
+ echo version is $version
+ sleep 1
  count=$(( count-1 ))
+ echo count is $count
  if [ $count -eq 0 ];
   then exit 1;
  fi;
